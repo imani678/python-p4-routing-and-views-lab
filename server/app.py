5c2329b86ba3241dab8bd1446a2c03dc7e9f2663
imani <18imani.vata@gmail.com>
2024-07-16 19:07:44 +0300

    final

--- server/app.py
+++ server/app.py
@@ -8,39 +8,36 @@ app = Flask(__name__)
 def index():
     return '<h1>Python Operations with Flask Routing and Views</h1>'
 
-@app.route('/print/<string>')
-def print_string(string):
-    print (string)
-    return f'{string}'
+@app.route('/print/<string:route>')
+def print_string(route):
+    print(route)
+    return route
 
-@app.route('/count/<int:integer>')
-def count(integer):
-    return "\n".join(str(i) for i in range(integer)) +"\n"
+@app.route('/count/<int:number>')
+def count(number):
+    count = f''
+    for n in range(number):
+        count += f'{n}\n'
+    return count
 
-@app.route('/math/<num1>/<operation>/<num2>')
-def math(num1, operation, num2):
-    try:
-        num1 = float(num1)
-        num2 = float(num2)
-    except ValueError:
-        return 'Error: Both parameters must be numbers', 400
-
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, num2, operation):
     if operation == '+':
-        result = num1 + num2
+        return str(num1 + num2)
+    
     elif operation == '-':
-        result = num1 - num2
-    elif operation == '*':
-        result = num1 * num2
-    elif operation == 'div':
-        result = num1 / num2 if num2 != 0 else 'Error: Division by zero'
-        if isinstance(result, float):
-            result = round(result, 1)  # Round to 1 decimal place
-    elif operation == '%':
-        result = num1 % num2
-    else:
-        return 'Error: Unsupported operation', 400
+        return str(num1 - num2)
 
-    return str(result)
+    elif operation == '*':
+        return str(num1 * num2)
+
+    elif operation == 'div':
+        return str(num1 / num2)
+
+    elif operation == '%':
+        return str(num1 % num2)
+
+    return 'Operation not recognized. Please use one of the following: + - * div %'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
